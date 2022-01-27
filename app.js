4ee90dfbd8a651eb38e6917751ffba508016f5e5
Ata Ahmad <ataahmad@Atas-MBP.lan>
2022-01-27 09:06:24 -0800

    migrated off ejs engine

--- app.js
+++ app.js
@@ -1,19 +1,48 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();
 const Note = require('./models/note');
 const notesRouter = require('./routes/notes');
 require('dotenv').config();
 
-app.set('view engine', 'ejs');
+const session = require('express-session');
+const { ExpressOIDC } = require('@okta/oidc-middleware');
+
+
+// app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
 
+app.use(session({
+  secret: 'this should be secure',
+  resave: true,
+  saveUninitialized: false
+}));
+
+
+const oidc = new ExpressOIDC({
+  issuer: `https://${process.env.OKTADOMAIN}/oauth2/default`,
+  client_id: `${process.env.CLIENTID}`,
+  client_secret: `${process.env.CLIENTSECRET}`,
+  appBaseUrl: 'http://localhost:3000',
+  redirect_uri: 'http://localhost:3000/authorization-code/callback',
+  scope: 'openid profile',
+  appBaseUrl: 'http://localhost:3000'
+});
+
+app.use(oidc.router);
 
 app.get('/', async (req, res) => {
+  console.log("Home Page loaded!");
   const notes = await Note.find().sort('-createdAt');
-  res.render('index', { notes: notes });
+  // res.render('index', { notes: notes });
+  res.sendFile(path.join(__dirname, '/html', 'index.html'));
+
 });
+
+app.all('*', oidc.ensureAuthenticated());
+
 
 mongoose.connect(process.env.SERVER, {
   useNewUrlParser: true,
